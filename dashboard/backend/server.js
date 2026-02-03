70a181f29334f5e2b071254e9e36e918a04febc6
Omelettae <jerrycheesyegg3@gmail.com>
2026-02-04 01:48:59 +0700

    Enhance logs API to accept date range parameters
    Updated the logs endpoint to support date range queries.

--- dashboard/backend/server.js
+++ dashboard/backend/server.js
@@ -42,21 +42,49 @@ app.get('/api/sensors', async (req, res) => {
   res.json(rows)
 })
 
-// GET logs for last X hours
-app.get('/api/logs', async (req, res) => {
-  const hours = req.query.hours || 2
+// // GET logs for last X hours
+// app.get('/api/logs', async (req, res) => {
+//   const hours = req.query.hours || 2
 
-  const [rows] = await pool.query(`
+//   const [rows] = await pool.query(`
+//     SELECT s.sensorID, s.sensorType, s.sensorLocation,
+//            l.datetime, l.temperature, l.humidity, l.windspeed, l.windDirection
+//     FROM SensorLog l
+//     JOIN Sensor s ON s.sensorID = l.sensorID
+//     WHERE l.datetime >= NOW() - INTERVAL ? HOUR
+//     ORDER BY l.datetime
+//   `, [hours])
+
+//   res.json(rows)
+// })
+
+app.get('/api/logs', async (req, res) => {
+  const { hours, start, end } = req.query
+
+  let query = `
     SELECT s.sensorID, s.sensorType, s.sensorLocation,
-           l.datetime, l.temperature, l.humidity, l.windspeed
+           l.datetime, l.temperature, l.humidity,
+           l.windspeed, l.windDirection, l.VPD
     FROM SensorLog l
     JOIN Sensor s ON s.sensorID = l.sensorID
-    WHERE l.datetime >= NOW() - INTERVAL ? HOUR
-    ORDER BY l.datetime
-  `, [hours])
+    WHERE 1=1
+  `
+  const params = []
 
+  if (start && end) {
+    query += ` AND l.datetime BETWEEN ? AND ?`
+    params.push(start, end)
+  } else {
+    query += ` AND l.datetime >= NOW() - INTERVAL ? HOUR`
+    params.push(hours || 6)
+  }
+
+  query += ` ORDER BY l.datetime`
+
+  const [rows] = await pool.query(query, params)
   res.json(rows)
 })
+
 
 
 // START SERVER
