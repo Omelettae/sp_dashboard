8aaeebcae4c96f0f0e1f3994755ef21c69ab116a
Korawit-chn <workingforbeingnothing@gmail.com>
2026-02-04 13:16:00 +0700

    Implement data logging endpoints for sensors
    Added new endpoints for DHT and C5A data logging.

--- dashboard/backend/server.js
+++ dashboard/backend/server.js
@@ -42,13 +42,13 @@ app.get('/api/sensors', async (req, res) => {
   res.json(rows)
 })
 
-// // GET logs for last X hours
+// GET logs for last X hours
 // app.get('/api/logs', async (req, res) => {
 //   const hours = req.query.hours || 2
 
 //   const [rows] = await pool.query(`
 //     SELECT s.sensorID, s.sensorType, s.sensorLocation,
-//            l.datetime, l.temperature, l.humidity, l.windspeed, l.windDirection
+//            l.datetime, l.temperature, l.humidity, l.windspeed, l.VPD
 //     FROM SensorLog l
 //     JOIN Sensor s ON s.sensorID = l.sensorID
 //     WHERE l.datetime >= NOW() - INTERVAL ? HOUR
@@ -85,6 +85,75 @@ app.get('/api/logs', async (req, res) => {
   res.json(rows)
 })
 
+app.post('/api/getDataDHT', async (req, res) => {
+  try {
+    const { sensorID, temperature, humidity, VPD, time } = req.body;
+
+    if (sensorID == null || temperature == null || humidity == null || VPD == null || time == null ) {
+      return res.status(400).json({ message: 'Missing DHT data' });
+    }
+
+    const sql = `
+      INSERT INTO SensorLog
+      (sensorID, datetime, temperature, humidity, VPD)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+
+    const [result] = await pool.execute(sql, [
+      sensorID,
+      time,
+      temperature,
+      humidity,
+      VPD
+    ]);
+
+    res.status(200).json({
+      success: true,
+      logID: result.insertId
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
+
+app.post('/api/getDataC5A', async (req, res) => {
+  try {
+    const { sensorID, windSpeed, windDirection, time } = req.body;
+
+    if (
+      sensorID == null ||
+      windSpeed == null ||
+      windDirection == null ||
+      time == null
+    ) {
+      return res.status(400).json({ message: 'Missing C5A data' });
+    }
+
+    const sql = `
+      INSERT INTO SensorLog
+      (sensorID, datetime, windspeed, windDirection)
+      VALUES (?, ?, ?, ?)
+    `;
+
+    const [result] = await pool.execute(sql, [
+      sensorID,
+      time,
+      windSpeed,
+      windDirection,
+    ]);
+
+    res.status(200).json({
+      success: true,
+      logID: result.insertId
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ success: false, error: err.message });
+  }
+});
 
 
 // START SERVER
